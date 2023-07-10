55e3e70693dea8cc669f2465d1e0cec2d2fc7aee
SirHoog <121210119+SirHoog@users.noreply.github.com>
2023-07-09 21:44:32 -0700

    added check

--- main.cpp
+++ main.cpp
@@ -207,6 +207,8 @@ SDL_FPoint projVert(std::array<float, 3> vert)
         camTransform
     )[0];
 
+    std::cout << transformedVert[2] == 0 << std::endl;
+
     // Perform projection calculations
     SDL_FPoint proj =
     {
